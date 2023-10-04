876fb05fc88e74f84aa34aee0b7b9e9edc0142f0
woojin <wjin0728@naver.com>
2023-10-04 21:23:43 +0900

    반복적으로 설정되는 랜덤 손 위치로 캐릭터가 이동하는거 구현 완료 t가 1을 넘으면 손 위치가 새로운 위치로 재설정 됨

--- Drill05.py
+++ Drill05.py
@@ -35,11 +35,16 @@ def reset_world():
     cx, cy = TUK_WIDTH // 2, TUK_HEIGHT // 2
     frame = 0
 
+    set_new_target_arrow()
+    action = 3
+
+
+def set_new_target_arrow():
+    global sx, sy, hx, hy, t
     sx, sy = cx, cy  # p1 : 시작점
     # hx, hy = TUK_WIDTH - 50, TUK_HEIGHT - 50
     hx, hy = random.randint(0, TUK_WIDTH), random.randint(0, TUK_HEIGHT)  # p2 : 끝점
     t = 0.0
-    action =3
 
 
 def render_world():
@@ -53,17 +58,21 @@ def render_world():
 def update_world():
     global frame
     global cx, cy
+    global sx, sy
+    global hx, hy
     global action
     global t
     frame = (frame + 1) % 8
 
     action = 1 if cx < hx else 0
 
-    if t< 1.0:
-        cx = (1-t)*sx + t*hx  # cx는 시작 x와 끝 x 를 1-t : t의 비율로 섞은 위치
-        cy = (1-t)*sy + t*hy
+    if t < 1.0:
+        cx = (1 - t) * sx + t * hx  # cx는 시작 x와 끝 x 를 1-t : t의 비율로 섞은 위치
+        cy = (1 - t) * sy + t * hy
         t += 0.001
-
+    else:
+        cx, cy = hx, hy
+        set_new_target_arrow()
 
 open_canvas(TUK_WIDTH, TUK_HEIGHT)
 hide_cursor()
