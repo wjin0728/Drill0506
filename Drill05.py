6f2e80553fcbb32bfb23b572d4442100ebfe20dc
woojin <wjin0728@naver.com>
2023-10-04 21:33:30 +0900

    frame 초기화 설정 추가

--- Drill05.py
+++ Drill05.py
@@ -26,25 +26,26 @@ def handle_events():
 
 def reset_world():
     global running, cx, cy, frame
-    global hx, hy
-    global sx, sy
-    global t
     global action
 
     running = True
     cx, cy = TUK_WIDTH // 2, TUK_HEIGHT // 2
     frame = 0
-
-    set_new_target_arrow()
     action = 3
+    set_new_target_arrow()
+
 
 
 def set_new_target_arrow():
     global sx, sy, hx, hy, t
+    global action
+    global frame
     sx, sy = cx, cy  # p1 : 시작점
     # hx, hy = TUK_WIDTH - 50, TUK_HEIGHT - 50
     hx, hy = random.randint(0, TUK_WIDTH), random.randint(0, TUK_HEIGHT)  # p2 : 끝점
     t = 0.0
+    action = 1 if sx < hx else 0
+    frame = 0
 
 
 def render_world():
@@ -64,7 +65,6 @@ def update_world():
     global t
     frame = (frame + 1) % 8
 
-    action = 1 if cx < hx else 0
 
     if t < 1.0:
         cx = (1 - t) * sx + t * hx  # cx는 시작 x와 끝 x 를 1-t : t의 비율로 섞은 위치
